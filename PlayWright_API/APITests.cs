5094f104af763c534f5cbe0e6fd160f3173baa4d
LoveMyDaughter <tsav1986@gmail.com>
2022-12-13 02:59:59 +0200

    Create FC test

--- PlayWright_API/APITests.cs
+++ PlayWright_API/APITests.cs
@@ -5,6 +5,7 @@ namespace PlayWright_API
     [TestFixture]
     public class APITests
     {
+/*      
         [Test]
         public async Task APIGetUnitCollection()
         {
@@ -55,7 +56,32 @@ namespace PlayWright_API
             var data = await response.JsonAsync();
             Console.WriteLine(data);
         }
+*/
 
+        [Test]
+        public async Task FCGetToken()
+        {
+            var playwright = await Playwright.CreateAsync();
 
+            var requestContext = await playwright.APIRequest.NewContextAsync(new APIRequestNewContextOptions()
+            {
+                BaseURL = "https://fctstint1602.qdev.net:60112/",
+               // IgnoreHTTPSErrors = true
+
+            });
+
+            var response = await requestContext.PostAsync(url: "tst_flowcal/identity/connect/token", new APIRequestContextOptions()
+            {
+                DataObject = new
+                {
+                    grant_type = "client_credentials",
+                    client_id = "flowcalclient",
+                    client_secret = "F621F470-9731-4A25-80EF-67A6F7C5F4B8",
+                    scope = "api"
+                }
+            });
+            var data = await response.JsonAsync();
+            Console.WriteLine(data);
+        }
     }
 }
